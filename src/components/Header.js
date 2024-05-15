10014263c254cc9990f956e91c242ef537372294
Cameron Swan <swanc@me.com>
2024-05-14 23:25:24 -0300

    header stuff.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,9 @@ import { Link } from "react-router-dom";
 export function Header() {
     return (
         <>
-        <header class="flex justify-between">
-        <img src="" alt=""/>
-        <h1>Cameron Swan</h1>
-        <nav>
+        <header class="flex justify-between align-middle p-10 border border-red-600">
+        <h1 class="text-5xl"><i class="fa-solid fa-location-dot"></i> Cameron Swan</h1>
+        <nav class="flex justify-center">
             <ul class="flex">
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/about'}>About</Link></li>
